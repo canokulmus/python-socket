0518f049adc1eae95b5264c599513afecbec8ae9
CAN OKULMUS <e2400265@ceng.metu.edu.tr>
2022-12-22 00:37:38 +0300

    22.12-00.37

--- client.py
+++ client.py
@@ -2,10 +2,34 @@
 
 import socket
 
+
+HEADERSIZE = 10
+
+
 # create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET = IPv4, SOCK_STREAM = TCP # also can use SOCK_DGRAM for UDP
 
 s.connect((socket.gethostname(), 1234)) # connect to the server
 
-msg = s.recv(1024) # receive 1024 bytes of data. This is the maximum amount of data that can be received at once (buffer size) # 1024 is a common buffer size
-print(msg.decode("utf-8")) # decode the message from bytes to a string
+while True:
+    
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16) # receive 1024 bytes of data. This is the maximum amount of data that can be received at once (buffer size) # 1024 is a common buffer size
+        
+        if new_msg:
+            print(f"new message length: {msg[:HEADERSIZE]}") # print the length of the message
+            msglen = int(msg[:HEADERSIZE]) # get the length of the message
+            new_msg = False 
+        full_msg += msg.decode("utf-8") # decode the message from bytes to string
+        
+        # if the length of the message is equal to the length of the message we received, then we have received the full message
+        if len(full_msg)-HEADERSIZE == msglen:  
+            print("full msg received")
+            print(full_msg[HEADERSIZE:]) # print the message without the header
+            new_msg = True
+            full_msg = ''
+            
+        
+    print(full_msg)
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 # socket server example in python
 
 import socket
+import time
+
+HEADERSIZE = 10
 
 # create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET = IPv4, SOCK_STREAM = TCP # also can use SOCK_DGRAM for UDP
@@ -18,5 +21,17 @@ while True:
     # clientsocket is a new socket object usable to send and receive data on the connection, and addr is the address bound to the socket on the other end of the connection
     clientsocket, address = s.accept() # accept returns a tuple of two values: a new socket object representing the connection, and the address of the client
     print(f"Connection from {address} has been established!")
+    
+    msg = "Welcome to the server!"
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg 
+
     # send a message to the client socket
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+    clientsocket.send(bytes(msg, "utf-8"))
+
+    while True: # send the time every 3 seconds
+        time.sleep(3)
+        msg = f"The time is! {time.time()}"
+        msg = f'{len(msg):<{HEADERSIZE}}' + msg 
+        clientsocket.send(bytes(msg, "utf-8"))
+        
+        
